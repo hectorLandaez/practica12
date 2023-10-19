549bf5f677fb7b422b2ce8dcccda28dd99818c6e
hectorLandaez <143516248+hectorLandaez@users.noreply.github.com>
2023-10-18 22:15:09 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : "https://github.com/hectorLandaez/practica12"
+  base : "https://hectorLandaez.github.io/practica12/"
 })
